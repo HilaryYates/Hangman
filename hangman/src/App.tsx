eb18d9a0c5abe2cb977f995f310f7a029fe59e46
hilaryyates <hilryyates@gmail.com>
2023-02-17 11:05:26 -0500

    drawing styles

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import words from "./wordList.json";
 import "./App.css";
+import { HangmanDrawing } from "./HangmanDrawing/HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 const App = () => {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
-  return <div></div>;
+  return (
+    <div className='container'>
+      <h1 className='win-or-lose-text'>Win Lose</h1>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 };
 
 export default App;
---
+++ hangman/src/HangmanDrawing/HangmanDrawing.tsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+export const HangmanDrawing = () => {
+  const HEAD = <div className='head'></div>;
+  const BODY = <div className='body'></div>;
+  const RIGHT_ARM = <div className='right-arm'></div>;
+  const LEFT_ARM = <div className='left-arm'></div>;
+  const RIGHT_LEG = <div className='right-leg'></div>;
+  const LEFT_LEG = <div className='left-leg'></div>;
+
+  // const LEFT_ARM = <div className='body'></div>;
+
+  return (
+    <div className='container'>
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
+      <div className='top-line' />
+      <div className='pole' />
+      <div className='upright-line' />
+      <div className='bottom-bar' />
+    </div>
+  );
+};
